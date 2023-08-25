ac01f2f2af9517deda5092ef9590c0d3555ecf13
Drew Fisher <drew@pods.media>
2023-08-25 09:16:44 -0400

    add impl upgrade script

---
+++ script/DeployCreatorImplOnly.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+
+import {ZoraDeployerBase} from "./ZoraDeployerBase.sol";
+import {ChainConfig, Deployment} from "../src/deployment/DeploymentConfig.sol";
+
+import {Zora1155Factory} from "../src/proxies/Zora1155Factory.sol";
+import {ZoraCreator1155Impl} from "../src/nft/ZoraCreator1155Impl.sol";
+
+contract DeployScript is ZoraDeployerBase {
+    function run() public {
+        ChainConfig memory chainConfig = getChainConfig();
+
+        console2.log("zoraFeeRecipient", chainConfig.mintFeeRecipient);
+        console2.log("factoryOwner", chainConfig.factoryOwner);
+        console2.log("protocolRewards", chainConfig.protocolRewards);
+
+        address deployer = vm.envAddress("DEPLOYER");
+        address factoryProxyAddress = vm.envAddress("FACTORY_PROXY");
+        console2.log("Deployer", deployer);
+
+        vm.startBroadcast(deployer);
+
+        Zora1155Factory factoryProxy = Zora1155Factory(payable(factoryProxyAddress));
+
+        ZoraCreator1155Impl creatorImpl = new ZoraCreator1155Impl(chainConfig.mintFeeRecipient, address(factoryProxy), chainConfig.protocolRewards);
+
+        console2.log("creatorImpl", address(creatorImpl));
+    }
+}
